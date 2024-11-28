c0b2446060ff45853973115395116d74c58a7bb6
Zalmai-Zazai <shahzalmai98@gmail.com>
2024-11-28 14:48:04 -0800

    first commit after completion

---
+++ src/app/posts/create/page.js
@@ -0,0 +1,57 @@
+'use client';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+
+const CreatePost = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const router = useRouter();
+
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:5004/posts', { title, content });
+      router.push('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="container mt-4">
+      <h1 className="text-center">Create Post</h1>
+
+      <form onSubmit={handelSubmit}>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput1" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">
+            Content
+          </label>
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary">Create</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
---
+++ src/app/posts/page.js
@@ -0,0 +1,235 @@
+'use client';
+import axios from 'axios';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [currentPostId, setCurrentPostId] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:5004/posts');
+      const sortedPosts = response.data.sort((a, b) =>
+        a.title.localeCompare(b.title)
+      );
+      setPosts(sortedPosts);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handelDelete = async (id) => {
+    // const currentPost = posts.find((post) => post.id === id);
+
+    // if (currentPost) {
+    await axios.delete(`http://localhost:5004/posts/${currentPostId}`);
+  };
+
+  const handelUpdate = async (e) => {
+    e.preventDefault();
+    //   await axios.put(`http://localhost:5004/posts/`, { title, content });
+    await axios.put(`http://localhost:5004/posts/${currentPostId}`, {
+      title,
+      content,
+    });
+  };
+  const filteredPosts = posts.filter((post) =>
+    post.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+  const openModal = (post) => {
+    setCurrentPostId(post.id);
+    setTitle(post.title);
+    setContent(post.content);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [posts]);
+
+  return (
+    <div className="container">
+      <div className="mt-4 d-flex  justify-content-between ">
+        <h1>All Posts</h1>
+        <input
+          type="text"
+          className="form-control w-25"
+          placeholder="Search by title"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <Link className="btn btn-primary " href={'/posts/create'}>
+          Create Post
+        </Link>
+      </div>
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredPosts.map((post) => (
+            <tr key={post.id}>
+              <th scope="row">{post.id}</th>
+              <td>{post.title}</td>
+              <td>{post.content}</td>
+              <td>
+                <button
+                  type="button"
+                  className="btn btn-success mx-3"
+                  data-bs-toggle="modal"
+                  data-bs-whatever="@mdo"
+                  data-bs-target={`#updateModal-${post.id}`}
+                  onClick={() => openModal(post)}
+                >
+                  Update
+                </button>
+
+                <div
+                  className="modal fade"
+                  id={`updateModal-${post.id}`}
+                  tabIndex="-1"
+                  aria-labelledby={`exampleModalLabel-${post.id}`}
+                  aria-hidden="true"
+                >
+                  <div className="modal-dialog">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h1
+                          className="modal-title fs-5"
+                          id={`exampleModalLabel-${post.id}`}
+                        >
+                          Update record: {post.id}
+                        </h1>
+                        <button
+                          type="button"
+                          className="btn-close"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
+                      </div>
+                      <form onSubmit={handelUpdate}>
+                        <div className="modal-body">
+                          <div className="mb-3">
+                            <label
+                              htmlFor="recipient-name"
+                              className="col-form-label"
+                            >
+                              Title:
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="recipient-name"
+                              value={title}
+                              onChange={(e) => {
+                                setTitle(e.target.value);
+                              }}
+                            />
+                          </div>
+                          <div className="mb-3">
+                            <label
+                              htmlFor="message-text"
+                              className="col-form-label"
+                            >
+                              Content:
+                            </label>
+                            <textarea
+                              className="form-control"
+                              id="message-text"
+                              value={content}
+                              onChange={(e) => {
+                                setContent(e.target.value);
+                              }}
+                            ></textarea>
+                          </div>
+                        </div>
+                        <div className="modal-footer">
+                          <button
+                            type="button"
+                            className="btn btn-secondary"
+                            data-bs-dismiss="modal"
+                          >
+                            Close
+                          </button>
+                          <button
+                            className="btn btn-primary"
+                            data-bs-dismiss="modal"
+                          >
+                            Update
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+
+                {/* Start of delete button and modal */}
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  data-bs-toggle="modal"
+                  data-bs-target={`#deleteModal-${post.id}`}
+                  onClick={() => openModal(post)}
+                >
+                  Delete
+                </button>
+                <div
+                  className="modal fade"
+                  aria-hidden="true"
+                  id={`deleteModal-${post.id}`}
+                  tabIndex="-1"
+                  aria-labelledby={`exampleModalLabel-${post.id}`}
+                >
+                  <div className="modal-dialog">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h1 className="modal-title fs-5" id="exampleModalLabel">
+                          Do you want delete record: {post.id}?
+                        </h1>
+                        <button
+                          type="button"
+                          className="btn-close"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
+                      </div>
+                      <div className="modal-body">...</div>
+                      <div className="modal-footer">
+                        <button
+                          type="button"
+                          className="btn btn-secondary"
+                          data-bs-dismiss="modal"
+                        >
+                          Close
+                        </button>
+                        <button
+                          onClick={() => handelDelete()}
+                          type="button"
+                          className="btn btn-primary"
+                          data-bs-dismiss="modal"
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Posts;
